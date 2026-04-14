842179412e554d0e35e52dd5d07ebff8c3ce4803
Aditi Joshi <aditijoshi2463@gmail.com>
2026-04-14 15:19:00 +0530

    Fixed MongoDB connection using env variables and improved security

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,11 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const dns = require("dns");//Added given below for the error it was not getting connecting... 
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const connectDB = async () => {
     await mongoose.connect(
-        "mongodb+srv://aditijoshi2463:8iTbecT4j4oaXQye@hellonode.6ix2los.mongodb.net/devtinder",
+        process.env.MONGO_URI
     )
 }
 
